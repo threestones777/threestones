ab80b0b35758fb385f809ad674d75f1be89262aa
threestones777 <40062290+threestones777@users.noreply.github.com>
2018-11-24 20:46:25 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
   <div id="app">
-    <router-link to="/home">home</router-link>
-    <router-link to="/one">one</router-link>
-    <router-link to="/two">two</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#/home">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/one">one</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/two">two</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown link
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -18,7 +44,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/home.vue
+++ src/components/home.vue
@@ -51,9 +51,15 @@ export default {
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.carousel-inner {
+  text-align: center;
+  background:orange;
+}
+.carousel-inner img {
+  width: 61%;           
+}
 h1, h2 {
   font-weight: normal;
 }
